d32dfa06b9bc87126888f14335c51df9a19c41db
Michael Bauer <michael.mi.bauer@partner.bmw.de>
2013-09-30 10:39:20 +0200

    initial commit of yamaica base core plugin

---
+++ plugins/de.bmw.yamaica.base.core/src/de/bmw/yamaica/base/core/internal/resourceproperties/YamaicaXmlConstants.java
@@ -0,0 +1,16 @@
+/* Copyright (C) 2013 BMW Group
+ * Author: Manfred Bathelt (manfred.bathelt@bmw.de)
+ * Author: Juergen Gehring (juergen.gehring@bmw.de)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+package de.bmw.yamaica.base.core.internal.resourceproperties;
+
+public interface YamaicaXmlConstants
+{
+    public static final String ROOT_TAG_NAME       = "yamaica";
+    public static final String RESOURCE_TAG_NAME   = "resource";
+    public static final String PROPERTY_TAG_NAME   = "property";
+    public static final String PATH_ATTRIBUTE_NAME = "path";
+    public static final String NAME_ATTRIBUTE_NAME = "name";
+}
---
+++ plugins/de.bmw.yamaica.base.core/src/de/bmw/yamaica/base/core/utils/AbstractPropertyChangeSupport.java
@@ -0,0 +1,54 @@
+/* Copyright (C) 2013 BMW Group
+ * Author: Manfred Bathelt (manfred.bathelt@bmw.de)
+ * Author: Juergen Gehring (juergen.gehring@bmw.de)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+package de.bmw.yamaica.base.core.utils;
+
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
+
+/**
+ * This class is to be used as base class to equip the extended class with
+ * Java beans property change support.
+ */
+public abstract class AbstractPropertyChangeSupport implements IPropertyChangeSupport
+{
+    protected PropertyChangeSupport propertyChangeSupport = new PropertyChangeSupport(this);
+
+    public synchronized void addPropertyChangeListener(PropertyChangeListener arg0)
+    {
+        propertyChangeSupport.addPropertyChangeListener(arg0);
+    }
+
+    public synchronized void addPropertyChangeListener(String arg0, PropertyChangeListener arg1)
+    {
+        propertyChangeSupport.addPropertyChangeListener(arg0, arg1);
+    }
+
+    public synchronized PropertyChangeListener[] getPropertyChangeListeners()
+    {
+        return propertyChangeSupport.getPropertyChangeListeners();
+    }
+
+    public synchronized PropertyChangeListener[] getPropertyChangeListeners(String arg0)
+    {
+        return propertyChangeSupport.getPropertyChangeListeners(arg0);
+    }
+
+    public synchronized boolean hasListeners(String arg0)
+    {
+        return propertyChangeSupport.hasListeners(arg0);
+    }
+
+    public synchronized void removePropertyChangeListener(PropertyChangeListener arg0)
+    {
+        propertyChangeSupport.removePropertyChangeListener(arg0);
+    }
+
+    public synchronized void removePropertyChangeListener(String arg0, PropertyChangeListener arg1)
+    {
+        propertyChangeSupport.removePropertyChangeListener(arg0, arg1);
+    }
+}
