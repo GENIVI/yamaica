cc68df441778913b14547f98139ff2302b8d9ee8
Michael Bauer <michael.mi.bauer@partner.bmw.de>
2013-09-30 11:00:55 +0200

    initial commit of yamaica base ui workbenchobserver plugin

---
+++ plugins/de.bmw.yamaica.base.ui.workbenchobserver/src/de/bmw/yamaica/base/ui/workbenchobserver/Preferences.java
@@ -0,0 +1,25 @@
+/* Copyright (C) 2013 BMW Group
+ * Author: Manfred Bathelt (manfred.bathelt@bmw.de)
+ * Author: Juergen Gehring (juergen.gehring@bmw.de)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+package de.bmw.yamaica.base.ui.workbenchobserver;
+
+import org.eclipse.jface.preference.IPreferenceStore;
+
+import de.bmw.yamaica.base.ui.internal.workbenchobserver.Activator;
+
+public class Preferences
+{
+    public static final String MENU_BAR_HIDING_RULE = "MENU_BAR_HIDING_RULE";
+    public static final String ALWAYS_HIDE          = "ALWAYS_HIDE";
+    public static final String NEVER_HIDE           = "NEVER_HIDE";
+    public static final String YAMAICA_HIDE         = "YAMAICA_HIDE";
+
+    // Enable others to access the internal preference store
+    public static IPreferenceStore getPreferenceStore()
+    {
+        return Activator.getDefault().getPreferenceStore();
+    }
+}
