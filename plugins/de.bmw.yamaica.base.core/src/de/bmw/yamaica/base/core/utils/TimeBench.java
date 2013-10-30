534d10714c3bbcc2e4b51620ee67611c49e7733e
Johannes Langlotz <johannes.langlotz@partner.bmw.de>
2013-10-30 12:38:32 +0100

    Added possibility to measure duration of method calls.

--- plugins/de.bmw.yamaica.base.core/src/de/bmw/yamaica/base/core/utils/TimeBench.java
+++ plugins/de.bmw.yamaica.base.core/src/de/bmw/yamaica/base/core/utils/TimeBench.java
@@ -25,7 +25,7 @@ public class TimeBench
 
         if (!timestamps.containsKey(id))
         {
-            timestampList = new LinkedList<TimeBench.Timestamp>();
+            timestampList = new LinkedList<Timestamp>();
 
             timestamps.put(id, timestampList);
         }
@@ -37,7 +37,7 @@ public class TimeBench
         timestampList.add(new Timestamp(description));
     }
 
-    public static void resetTimer(int id)
+    public static void clearTimeStamps(int id)
     {
         if (null != timestamps)
         {
@@ -45,7 +45,7 @@ public class TimeBench
         }
     }
 
-    public static void resetAllTimers()
+    public static void clearAllTimeStamps()
     {
         if (null != timestamps)
         {
@@ -68,6 +68,7 @@ public class TimeBench
             {
                 Timestamp firstTimestamp = timestampList.get(0);
                 Timestamp previousTimestamp = timestampList.get(0);
+                boolean skippedTimestamp = false;
 
                 System.out.printf("%n######################## Timestamps of ID %-3d ########################%n", id);
                 System.out.printf("  |Description                             |    Duration|    Absolute|%n");
@@ -83,10 +84,14 @@ public class TimeBench
                         long absoluteTime = currentTime - firstTimestamp.getTimestamp();
 
                         System.out.printf(" - %-40s %,12d %,12d%n", description, durationTime, absoluteTime);
+
+                        skippedTimestamp = false;
                     }
-                    else if (durationTime > 0)
+                    else if (durationTime > 0 && false == skippedTimestamp)
                     {
                         System.out.printf(" - ...%n");
+
+                        skippedTimestamp = true;
                     }
 
                     previousTimestamp = currentTimestamp;
@@ -97,8 +102,8 @@ public class TimeBench
 
     protected static class Timestamp
     {
-        protected final String description;
-        protected final long   timestamp;
+        private final String description;
+        private final long   timestamp;
 
         public Timestamp(String description)
         {
@@ -114,6 +119,127 @@ public class TimeBench
         public long getTimestamp()
         {
             return timestamp;
+        }
+    }
+
+    /*****************************************************************************/
+
+    protected static Map<Integer, Timer> timers;
+
+    public static void startTimer(int id, String description)
+    {
+        if (null == timers)
+        {
+            timers = new HashMap<Integer, Timer>();
+        }
+
+        Timer timer;
+
+        if (!timers.containsKey(id))
+        {
+            timer = new Timer(description);
+
+            timers.put(id, timer);
+        }
+        else
+        {
+            timer = timers.get(id);
+        }
+
+        timer.start();
+    }
+
+    public static void stopTimer(int id)
+    {
+        if (null != timers && timers.containsKey(id))
+        {
+            timers.get(id).stop();
+        }
+    }
+
+    public static void clearTimer(int id)
+    {
+        if (null != timers)
+        {
+            timers.remove(id);
+        }
+    }
+
+    public static void clearAllTimers()
+    {
+        if (null != timers)
+        {
+            timers.clear();
+        }
+    }
+
+    public static void printAllTimers()
+    {
+        System.out.printf("%n########################### Details of Timers ###########################%n");
+        System.out.printf("|Description                             |        Time|    Runs|Time/Run|%n");
+
+        if (null != timers)
+        {
+            for (int id : timers.keySet())
+            {
+                Timer timer = timers.get(id);
+                long totalTime = timer.getTotalTime();
+                int runs = timer.getRuns();
+                long timePerRun = totalTime / runs;
+
+                System.out.printf(" %-40s %,12d %,8d %,8d%n", timer.getDescription(), totalTime, runs, timePerRun);
+            }
+        }
+    }
+
+    protected static class Timer
+    {
+        private static final int STOPPED       = 0;
+        private static final int STARTED       = 1;
+
+        private final String     description;
+        private long             totalTime     = 0;
+        private long             lastStartTime = 0;
+        private int              runs          = 0;
+        private int              state         = STOPPED;
+
+        public Timer(String description)
+        {
+            this.description = description;
+        }
+
+        public void start()
+        {
+            if (STOPPED == state)
+            {
+                state = STARTED;
+                lastStartTime = System.currentTimeMillis();
+            }
+        }
+
+        public void stop()
+        {
+            if (STARTED == state)
+            {
+                totalTime += System.currentTimeMillis() - lastStartTime;
+                runs++;
+                state = STOPPED;
+            }
+        }
+
+        public String getDescription()
+        {
+            return description;
+        }
+
+        public long getTotalTime()
+        {
+            return totalTime;
+        }
+
+        public int getRuns()
+        {
+            return runs;
         }
     }
 }
