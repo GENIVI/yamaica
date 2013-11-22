27da205804e1283eba9e12c708e209ba857919dd
Johannes Langlotz <johannes.langlotz@partner.bmw.de>
2013-11-22 16:33:22 +0100

    Latest test changes (draft).

--- tests/de.bmw.yamaica.base.tests/src/de/bmw/yamaica/base/tests/utils/EMFHelper.java
+++ tests/de.bmw.yamaica.base.tests/src/de/bmw/yamaica/base/tests/utils/EMFHelper.java
@@ -1,27 +1,14 @@
-/* Copyright (C) 2013 BMW Group
- * Author: Manfred Bathelt (manfred.bathelt@bmw.de)
- * Author: Juergen Gehring (juergen.gehring@bmw.de)
- * This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 package de.bmw.yamaica.base.tests.utils;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
 
-import java.io.File;
-import java.io.IOException;
-import java.net.URISyntaxException;
-import java.net.URL;
 import java.util.Collections;
 import java.util.HashSet;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Set;
 
-import org.eclipse.core.runtime.FileLocator;
 import org.eclipse.core.runtime.Path;
-import org.eclipse.core.runtime.Platform;
 import org.eclipse.emf.common.util.Diagnostic;
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.compare.Comparison;
@@ -32,55 +19,9 @@ import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.eclipse.emf.ecore.util.Diagnostician;
-import org.osgi.framework.Bundle;
 
-public class TransformationAdapterBase
+public class EMFHelper
 {
-    public static URI getFileUriFromBundleRelativePath(String bundleId, String bundleRelativePath) throws URISyntaxException, IOException
-    {
-        Bundle bundle = Platform.getBundle(bundleId);
-        URL url = FileLocator.find(bundle, new Path(""), null);
-
-        // Append bundle relative path after resolving URL to allow the creation of URI to an non existing file or folder.
-        return URI.createURI(FileLocator.resolve(url).toURI().toString() + bundleRelativePath);
-    }
-
-    public static List<File> getFilesOfDirectory(File directory)
-    {
-        List<File> fileList = new LinkedList<File>();
-
-        if (!directory.isDirectory())
-        {
-            return fileList;
-        }
-
-        for (File fileInDirectory : directory.listFiles())
-        {
-            if (fileInDirectory.isDirectory())
-            {
-                fileList.addAll(getFilesOfDirectory(fileInDirectory));
-            }
-            else
-            {
-                fileList.add(fileInDirectory);
-            }
-        }
-
-        return fileList;
-    }
-
-    public static List<URI> toFileUriList(List<File> files)
-    {
-        List<URI> fileUriList = new LinkedList<URI>();
-
-        for (File file : files)
-        {
-            fileUriList.add(URI.createFileURI(file.getPath()));
-        }
-
-        return fileUriList;
-    }
-
     public static ResourceSet loadFilesIntoResourceSet(ResourceSet resourceSet, List<URI> files, String[] fileExtensions) throws Exception
     {
         Set<String> fileExtensionsSet = new HashSet<String>();
---
+++ tests/de.bmw.yamaica.base.tests/src/de/bmw/yamaica/base/tests/utils/PathHelper.java
@@ -0,0 +1,62 @@
+package de.bmw.yamaica.base.tests.utils;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.util.LinkedList;
+import java.util.List;
+
+import org.eclipse.core.runtime.FileLocator;
+import org.eclipse.core.runtime.Path;
+import org.eclipse.core.runtime.Platform;
+import org.eclipse.emf.common.util.URI;
+import org.osgi.framework.Bundle;
+
+public class PathHelper
+{
+    public static URI getFileUriFromBundleRelativePath(String bundleId, String bundleRelativePath) throws URISyntaxException, IOException
+    {
+        Bundle bundle = Platform.getBundle(bundleId);
+        URL url = FileLocator.find(bundle, new Path(""), null);
+
+        // Append bundle relative path after resolving URL to allow the creation of URI to an non existing file or folder.
+        return URI.createURI(FileLocator.resolve(url).toURI().toString() + bundleRelativePath);
+    }
+
+    public static List<File> getFilesOfDirectory(File directory)
+    {
+        List<File> fileList = new LinkedList<File>();
+
+        if (!directory.isDirectory())
+        {
+            return fileList;
+        }
+
+        for (File fileInDirectory : directory.listFiles())
+        {
+            if (fileInDirectory.isDirectory())
+            {
+                fileList.addAll(getFilesOfDirectory(fileInDirectory));
+            }
+            else
+            {
+                fileList.add(fileInDirectory);
+            }
+        }
+
+        return fileList;
+    }
+
+    public static List<URI> toFileUriList(List<File> files)
+    {
+        List<URI> fileUriList = new LinkedList<URI>();
+
+        for (File file : files)
+        {
+            fileUriList.add(URI.createFileURI(file.getPath()));
+        }
+
+        return fileUriList;
+    }
+}
